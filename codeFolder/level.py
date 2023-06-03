78a39b31a7dbcfb4fcb2daba3c90b3b3abdc877b
valentinkatanga <valentin.katanga@student.odisee.be>
2023-06-03 17:13:10 +0200

    .

--- codeFolder/level.py
+++ codeFolder/level.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
 	def __init__(self):
@@ -13,10 +15,15 @@ class Level:
 
 		#sprite setup
 		self.create_map()
+
 	def create_map(self):
-		for row in WORLD_MAP:
-			print(row)
+		for row_index,row in enumerate(WORLD_MAP):
+			for col_index, col in enumerate(row):
+				x = col_index * TILESIZE
+				y = row_index * TILESIZE
+				if col == 'x':
+					Tile((x,y),[self.visible_sprites])
 
 	def run(self):
-			# update and draw the game
-			pass
+		# update and draw the game
+		self.visible_sprites.draw(self.display_surface)
